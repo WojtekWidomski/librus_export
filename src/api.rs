2d4c10394bacad41e2c0f54cc3c1c77a6c246b44
Wojtek Widomski <98206524+WojtekWidomski@users.noreply.github.com>
2024-04-21 22:08:11 +0200

    Start implementing messages support

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Context, Result};
 use serde::Serialize;
+use serde_json::Value;
 
 const AUTH_URL1: &str =
     "https://api.librus.pl/OAuth/Authorization?client_id=46&response_type=code&scope=mydata";
@@ -7,11 +8,37 @@ const AUTH_URL2: &str = "https://api.librus.pl/OAuth/Authorization?client_id=46"
 const GRANT_URL: &str = "https://api.librus.pl/OAuth/Authorization/Grant?client_id=46";
 const MSG_URL: &str = "https://synergia.librus.pl/wiadomosci3";
 
+// There are only few, predefined E-mail folder in Librus
+pub enum MessagesFolder {
+    Inbox,
+    Sent,
+    Trash,
+    ArchiveInbox,
+    ArchiveSent,
+    ArchiveTrash,
+}
+
 #[derive(Serialize)]
 struct LoginData {
     action: String,
     login: String,
     pass: String,
+}
+
+struct User<'a> {
+    id: i32,
+    first_name: &'a str,
+    last_name: &'a str,
+}
+
+struct Message<'a> {
+    id: i32,
+    sender_first_name: &'a str,
+    sender_last_name: &'a str,
+    topic: &'a str,
+    content: Option<&'a str>,
+    send_date: &'a str,
+    receivers: Option<Vec<User<'a>>>,
 }
 
 pub struct SynergiaClient {
@@ -52,5 +79,56 @@ impl SynergiaClient {
         client.get(MSG_URL).send()?;
 
         Ok(SynergiaClient { client })
+    }
+
+    pub fn get_messages(&self, folder: MessagesFolder) -> Result<()> {
+        let folder_path = match folder {
+            MessagesFolder::Inbox => "inbox",
+            MessagesFolder::Sent => "outbox",
+            MessagesFolder::Trash => "trash-bin",
+            MessagesFolder::ArchiveInbox => "archive/inbox",
+            MessagesFolder::ArchiveSent => "archive/outbox",
+            MessagesFolder::ArchiveTrash => "archive/trash-bin",
+        };
+
+        // Get only first message, because API will return all messages count at the same time
+        let messages_res = self
+            .client
+            .get(format!(
+                "https://wiadomosci.librus.pl/api/{}/messages?page=1&limit=1",
+                folder_path
+            ))
+            .send()?;
+
+        let first_msg: Value = serde_json::from_str(messages_res.text()?.as_str())
+            .context("Message deserialization error")?;
+
+        let msg_count = &first_msg["total"]
+            .as_i64()
+            .context("Failed to get message number")?;
+
+        dbg!(msg_count);
+
+        // Get msg_count messages.
+        let messages_res = self
+            .client
+            .get(format!(
+                "https://wiadomosci.librus.pl/api/{}/messages?page=1&limit={}",
+                folder_path, msg_count
+            ))
+            .send()?;
+
+        let msg_list: Value = serde_json::from_str(messages_res.text()?.as_str())
+            .context("Messages deserialization error")?;
+
+        let converted_list = msg_list["data"]
+            .as_array()
+            .context("Message deserialization error")?
+            .iter()
+            .map(|msg| {
+                // TODO: Convert messages from api to structs.
+            });
+
+        Ok(())
     }
 }
