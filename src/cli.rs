1ad5f6feb857110ec3de7b47b8f7a86b8beb140c
WojtekWidomski <98206524+WojtekWidomski@users.noreply.github.com>
2024-04-27 11:03:56 +0200

    Ask for `min_big_groups`

--- src/cli.rs
+++ src/cli.rs
@@ -7,6 +7,20 @@ use crate::api::{
 };
 
 fn login() -> SynergiaClient {
+    println!("Some teachers often send messages to very large group of users");
+    println!("(e.g. all students in the school). This program can save such");
+    println!("groups in separate file and save only group id in file with");
+    println!("messages. This will result in smaller, and easier to browse files.");
+    println!("Enter number of users in group to be considered as large.");
+    println!("Use 0 to disable this feature.");
+
+    let min_big_groups: usize = Input::new()
+        .with_prompt("Min users in big group")
+        .default(10)
+        .show_default(true)
+        .interact()
+        .unwrap();
+
     println!("Log in to your LIBRUS Synergia account.");
     println!("Remember to use Synergia account and not LIBRUS mobile app account.");
     println!("No characters will be displayed in terminal while entering password.");
@@ -19,7 +33,8 @@ fn login() -> SynergiaClient {
 
         let password = Password::new().with_prompt("Password").interact().unwrap();
 
-        let client_result = SynergiaClient::login(username.as_str(), password.as_str(), 10);
+        let client_result =
+            SynergiaClient::login(username.as_str(), password.as_str(), min_big_groups);
 
         match client_result {
             std::result::Result::Ok(client) => {
